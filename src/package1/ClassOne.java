dc77188e099d1c7a0b75f31110b8f13e634e24ee
selenucar <slnucartx@gmail.com>
2021-08-01 14:40:36 -0500

    second commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -3,5 +3,6 @@ package package1;
 public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Batch 35 / 36 learn Github");
     }
 }
