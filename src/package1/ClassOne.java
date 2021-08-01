b7d05511fac641a122d55b2b60ab8343c5a57502
selenucar <slnucartx@gmail.com>
2021-08-01 14:42:39 -0500

    third commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -4,5 +4,6 @@ public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Batch 35 / 36 learn Github");
+        System.out.println("We like Git but we will like more when we have more practice ");
     }
 }
