c686b4848e5bdeb7aeb44b8024d405a435d775ba
selenucar <slnucartx@gmail.com>
2021-08-07 14:55:26 -0500

    commiting mine

--- src/package1/ClassTwo.java
+++ src/package1/ClassTwo.java
@@ -7,7 +7,7 @@ public class ClassTwo {
         System.out.println(sum);
 
         int y = 0;
-        
+        String str ="Java";
         
         System.out.println("This is remote repo code");
 
