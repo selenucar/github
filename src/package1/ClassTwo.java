9688f60f0e6c3e59ada3b91a9e2e8dbf93cdd9b0
selenucar <slnucartx@gmail.com>
2021-08-07 13:29:40 -0500

    Commiting ClassTwo

---
+++ src/package1/ClassTwo.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class ClassTwo {
+    public static void main(String[] args) {
+        int x = 5;
+        int sum = x+2;
+        System.out.println(sum);
+
+        int y = 0;
+
+    }
+
+
+
+}
